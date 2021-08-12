5a5f614df9e5e64952e0d77263867a3037de2017
Anthony Giles <omgliekpie@gmail.com>
2021-08-12 17:52:14 -0600

    add method to swap current player

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
 class Game
+  attr_reader :player
 
   def initialize(player = 'black')
     @player = player
     @board = Board.new
+  end
+
+  def swap_players!
+    @player == 'black' ? @player = 'white' : @player = 'black'
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,28 @@
 # frozen_string_literal: true
 
 require_relative '../lib/game'
+require_relative '../lib/board'
 
 describe Game do
 
+    describe '#swap_players!' do
+
+      context 'when black is the current player' do
+        subject(:game_with_player_black) { described_class.new }
+
+        it 'should change player to white' do
+          game_with_player_black.swap_players!
+          expect(game_with_player_black.player).to eq('white')
+        end
+      end
+
+      context 'when white is the current player' do
+        subject(:game_with_player_white) { described_class.new('white') }
+
+        it 'should change player to black' do
+          game_with_player_white.swap_players!
+          expect(game_with_player_white.player).to eq('black')
+        end
+      end
+    end
 end
