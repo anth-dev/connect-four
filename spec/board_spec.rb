82349e4fa1d7f7aea1e5fbebb6068bb62f209598
Anthony Giles <omgliekpie@gmail.com>
2021-08-13 11:14:14 -0600

    work on the display for the game

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -105,4 +105,35 @@ describe Board do
       end
     end
   end
+
+  describe '#determine_display_value' do
+    
+      context 'when the space is empty' do
+        subject(:board_for_testing_display) { described_class.new( [[black_token, black_token, nil, nil, nil, nil], [white_token, white_token, nil, nil, nil, nil], [white_token, nil, nil, nil, nil, nil], [black_token, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil]] ) }
+        let(:black_token) { instance_double("Token", :owner => 'black') }
+        let(:white_token) { instance_double("Token", :owner => 'white') }
+
+        it 'should return a string containing one space' do
+          expect(board_for_testing_display.determine_display_value(0, 2)).to eq(' ')
+        end
+      end
+
+      context 'when the space contains a white token' do
+        subject(:board_with_one_white_token) { described_class.new( [[white_token, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil]] ) }
+        let(:white_token) { instance_double("Token", :owner => 'white') }
+
+        it 'should return the unicode character of a white game token' do
+          expect(board_with_one_white_token.determine_display_value(0, 0)).to eq("\u26AA")
+        end
+      end
+
+      context 'when the space contains a black token' do
+        subject(:board_with_one_black_token) { described_class.new( [[black_token, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil]] ) }
+        let(:black_token) { instance_double("Token", :owner => 'black') }
+
+        it 'should return the unicode character of a black game token' do
+          expect(board_with_one_black_token.determine_display_value(0, 0)).to eq("\u26AB")
+        end
+      end
+  end
 end
